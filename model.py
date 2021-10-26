e4add91d1d3b43b73b34d6f25c398114e4d61572
KanakaP <49495944+farrellkanaka@users.noreply.github.com>
2021-10-26 18:18:21 +0700

    Update model.py

--- model.py
+++ model.py
@@ -6,34 +6,34 @@ class TripletLoss:
     def conv_net(self, x, reuse=False):
         with tf.name_scope("model"):
             with tf.variable_scope("conv1") as scope:
-                net = tf.contrib.layers.conv2d(x, 32, [7, 7], activation_fn=tf.nn.relu, padding='SAME',
+                net = tf.contrib.layers.conv2d(x, 64, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                               scope=scope, reuse=reuse)
-                net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+                                               scope=scope,normalizer_fn=tf.contrib.layers.batch_norm, reuse=reuse)
+                net = tf.contrib.layers.avg_pool2d(net, [3, 2], stride=[3,2], padding='SAME')
 
             with tf.variable_scope("conv2") as scope:
-                net = tf.contrib.layers.conv2d(net, 64, [5, 5], activation_fn=tf.nn.relu, padding='SAME',
-                                               weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                               scope=scope, reuse=reuse)
-                net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
-
-            with tf.variable_scope("conv3") as scope:
                 net = tf.contrib.layers.conv2d(net, 128, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                               scope=scope, reuse=reuse)
-                net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+                                               scope=scope,normalizer_fn=tf.contrib.layers.batch_norm, reuse=reuse)
+                net = tf.contrib.layers.avg_pool2d(net, [3, 2], stride=[3,2], padding='SAME')
+
+            with tf.variable_scope("conv3") as scope:
+                net = tf.contrib.layers.conv2d(net, 256, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
+                                               weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                               scope=scope,normalizer_fn=tf.contrib.layers.batch_norm, reuse=reuse)
+                net = tf.contrib.layers.avg_pool2d(net, [3, 2], stride=[3,2], padding='SAME')
 
             with tf.variable_scope("conv4") as scope:
-                net = tf.contrib.layers.conv2d(net, 256, [1, 1], activation_fn=tf.nn.relu, padding='SAME',
+                net = tf.contrib.layers.conv2d(net, 512, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
                                                weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                               scope=scope, reuse=reuse)
-                net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+                                               scope=scope,normalizer_fn=tf.contrib.layers.batch_norm, reuse=reuse)
+                net = tf.contrib.layers.avg_pool2d(net, [3, 2], stride=[3,2], padding='SAME')
 
             with tf.variable_scope("conv5") as scope:
-                net = tf.contrib.layers.conv2d(net, 28, [1, 1], activation_fn=None, padding='SAME',
+                net = tf.contrib.layers.conv2d(net, 1024, [3, 3], activation_fn=None, padding='SAME',
                                                weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                               scope=scope, reuse=reuse)
-                net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+                                               scope=scope,normalizer_fn=tf.contrib.layers.batch_norm, reuse=reuse)
+                net = tf.contrib.layers.avg_pool2d(net, [3, 2], stride=[3,2], padding='SAME')
 
             net = tf.contrib.layers.flatten(net)
 
