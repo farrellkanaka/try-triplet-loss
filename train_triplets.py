160662c21842fc9b0bec88969fb321cbf8d4aac2
KanakaP <49495944+farrellkanaka@users.noreply.github.com>
2021-10-27 11:42:17 +0700

    Update train_triplets.py

--- train_triplets.py
+++ train_triplets.py
@@ -5,7 +5,7 @@ from model import TripletLoss
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('batch_size', 512, 'Batch size.')
-flags.DEFINE_integer('train_iter', 2000, 'Total training iter')
+flags.DEFINE_integer('train_iter', 200, 'Total training iter')
 flags.DEFINE_integer('step', 50, 'Save after ... iteration')
 flags.DEFINE_float('learning_rate','0.01','Learning rate')
 flags.DEFINE_float('momentum','0.99', 'Momentum')
